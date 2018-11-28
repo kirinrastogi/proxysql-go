e42f62fdf0432538c8a564ecccedc8b1537c6ea3
kirinrastogi <kirin.rastogi@shopify.com>
2018-11-28 17:23:07 -0500

    add example code

--- example/example.go
+++ example/example.go
@@ -1,9 +1,39 @@
 package main
 
+// example application that runs along side ProxySQL
+
 import (
-	"fmt"
+	"github.com/kirinrastogi/proxysql-go"
+	"log"
 )
 
 func main() {
-	fmt.Println("hello world")
+	conn, err := proxysql.New("remote-admin:password@tcp(localhost:6032)/")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = conn.Clear()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = conn.AddHost(proxysql.Hostname("example"), proxysql.Hostgroup(1))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = conn.PersistChanges()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	entries, err := conn.All()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, entry := range entries {
+		log.Printf("entry: %v\n", entry)
+	}
 }
